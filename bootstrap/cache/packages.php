970fa0899f157c5bbdd92792037d2c52e674cd37
Md Jamal Uddin <43787665+Jamal161@users.noreply.github.com>
2021-01-02 22:29:09 +0600

    Add files via upload

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,127 @@
+<?php return array (
+  'barryvdh/laravel-dompdf' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Barryvdh\\DomPDF\\ServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'PDF' => 'Barryvdh\\DomPDF\\Facade',
+    ),
+  ),
+  'facade/ignition' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Facade\\Ignition\\IgnitionServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Flare' => 'Facade\\Ignition\\Facades\\Flare',
+    ),
+  ),
+  'fideloper/proxy' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
+    ),
+  ),
+  'fruitcake/laravel-cors' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fruitcake\\Cors\\CorsServiceProvider',
+    ),
+  ),
+  'haruncpi/laravel-simple-filemanager' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Haruncpi\\LaravelSimpleFilemanager\\ServiceProvider',
+    ),
+  ),
+  'intervention/image' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Intervention\\Image\\ImageServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Image' => 'Intervention\\Image\\Facades\\Image',
+    ),
+  ),
+  'laravel/socialite' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Socialite\\SocialiteServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Socialite' => 'Laravel\\Socialite\\Facades\\Socialite',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'laravel/ui' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Ui\\UiServiceProvider',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'spatie/laravel-newsletter' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\Newsletter\\NewsletterServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Newsletter' => 'Spatie\\Newsletter\\NewsletterFacade',
+    ),
+  ),
+  'srmklive/paypal' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Srmklive\\PayPal\\Providers\\PayPalServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'PayPal' => 'Srmklive\\PayPal\\Facades\\PayPal',
+    ),
+  ),
+  'unisharp/laravel-filemanager' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'UniSharp\\LaravelFilemanager\\LaravelFilemanagerServiceProvider',
+    ),
+    'aliases' => 
+    array (
+    ),
+  ),
+);
---
+++ config/newsletter.php
@@ -0,0 +1,51 @@
+<?php
+
+return [
+
+    /*
+     * The driver to use to interact with MailChimp API.
+     * You may use "log" or "null" to prevent calling the
+     * API directly from your environment.
+     */
+    'driver' => env('MAILCHIMP_DRIVER', 'api'),
+
+    /*
+     * The API key of a MailChimp account. You can find yours at
+     * https://us10.admin.mailchimp.com/account/api-key-popup/.
+     */
+    'apiKey' => env('ed7d38aae86a90c8866bb429ccea5fbd-us7'),
+
+    /*
+     * The listName to use when no listName has been specified in a method.
+     */
+    'defaultListName' => 'subscribers',
+
+    /*
+     * Here you can define properties of the lists.
+     */
+    'lists' => [
+
+        /*
+         * This key is used to identify this list. It can be used
+         * as the listName parameter provided in the various methods.
+         *
+         * You can set it to any string you want and you can add
+         * as many lists as you want.
+         */
+        'subscribers' => [
+
+            /*
+             * A MailChimp list id. Check the MailChimp docs if you don't know
+             * how to get this value:
+             * http://kb.mailchimp.com/lists/managing-subscribers/find-your-list-id.
+             */
+            'id' => env('e29786b967521bed29e73da0f0f42c0e-us7'),
+        ],
+    ],
+
+    /*
+     * If you're having trouble with https connections, set this to false.
+     */
+    'ssl' => true,
+
+];
